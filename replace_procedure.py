7a9a8782c28700331e0fd38fdce8d5ebeb937cd8
Carson Gedeus <carsongedeus@icloud.com>
2020-11-07 21:37:36 -0500

    add automatic_file_update class

--- replace_procedure.py
+++ replace_procedure.py
@@ -1,18 +1,27 @@
-#Test script to automate testing
-# May 16, 2020  -  Carson Gedeus
+# Script to automatically change file characters
+# and syntax.
 
 import re
+import os
 
-def main():
+class automatic_file_update:
 
-    filename = "/Users/carsongedeus/Desktop/test_file.txt"
+    def __init__(self, file_name):
+        self.file = file_name
 
-    with open(filename, "r") as file_handler1:
-        read_file = file_handler1.read()
-        replace_char = read_file.replace('.', '_')
-        
-    with open(filename, "w") as file_handler2:
-        file_write = file_handler2.write(replace_char)
+    def read_write(self):
+        '''
+        Read and update current file(s) with new characters and syntax
+        '''
+        with open(self.file, 'r') as file_handler1:
+            read_file = file_handler1.read()
+            replace_char = read_file.replace('.', '_')
 
-if __name__ == "__main__":
-    main()
+        with open(self.file, 'w') as file_handler2:
+            file_write = file_handler2.write(replace_char)
+            print file_write
+
+if __name__ == '__main__':
+
+    for root, dir, files in os.walk("C:\Users\cgede"):
+        automatic_file_update.read_write(files)
